0fad72210585d19326466877291cdaa854d8e9c8
Gloria <totallygloria@users.noreply.github.com>
2015-12-22 12:49:36 -0800

    Cleaned up spacing errors, changed to pythonic
    TIL (val == False) is less pythonic than (not val). Changed accordingly.

--- StackImplementation.py
+++ StackImplementation.py
@@ -5,110 +5,103 @@
 
 class Stack():
 	
-	def __init__(self):
-		self.items = []
-		
-	def push(self, item):
-		self.items.append(item)
-		return self.items
+    def __init__(self):
+        self.items = []
 	
-	def pop(self):
-		return self.items.pop()
+    def push(self, item):
+        self.items.append(item)
+            return self.items
+	
+    def pop(self):
+        return self.items.pop()
 		
-	def peek(self):
-		return self.items[-1]
+    def peek(self):
+        return self.items[-1]
 		
-	def isEmpty(self):
-		return self.items == []
+    def isEmpty(self):
+        return len(self.items) == 0
 		
-	def size(self):
-		return len(self.items)
+    def size(self):
+        return len(self.items)
 
 
 def rev_string(inputString):
 	
-	stack = Stack()
-	new_string = str()
+    stack = Stack()
+    new_string = str()
 	
-	for char in inputString:
-		stack.push(char)
+    for char in inputString:
+        stack.push(char)
 	
-	while stack.isEmpty() == False:
-		new_string += stack.pop()
-		
-	return new_string	
+    while not stack.isEmpty():
+        new_string += stack.pop()
+    return new_string	
 
 
 def paren_checker(string):
 	
-	stack = Stack()
+    stack = Stack()
 	
-	for char in string:
-		if char == '(':
-			stack.push(char)
-		elif char == ')':
-			if stack.isEmpty() == False:
-				stack.pop()
-			else:
-				return 'Unbalanced'
+    for char in string:
+        if char == '(':
+            stack.push(char)
+        elif char == ')':
+            if not stack.isEmpty():
+                stack.pop()
+            else:
+                return 'Unbalanced'
 	
-	if stack.isEmpty() == False:
-		return 'Unbalanced'
-	else:
-		return 'Balanced'
+    if not stack.isEmpty():
+        return 'Unbalanced'
+    else:
+        return 'Balanced'
 
 
 def general_checker(string):
 	
-	stack = Stack()
-	opens = '([{<'
-	closes = ')]}>'
+    stack = Stack()
+    opens = '([{<'
+    closes = ')]}>'
 	
-	for char in string:
-		if char in opens:
-			stack.push(char)
-		elif char in closes:
-			if stack.isEmpty():
-				return "Stack prematurely empty. Unbalanced."
-			else:
-				prior = stack.pop()
-				return match_checker(char,prior) #returns Balanced or Unbalanced
-				
-				
-	if stack.isEmpty():
-		return 'Balanced'
-	else:
-		return 'Unbalanced'
+    for char in string:
+        if char in opens:
+            stack.push(char)
+        elif char in closes:
+            if stack.isEmpty():
+                return "Stack prematurely empty. Unbalanced."
+            else:
+                prior = stack.pop()
+                return match_checker(char,prior) #returns Balanced or Unbalanced
+    if stack.isEmpty():
+        return 'Balanced'
+    else:
+        return 'Unbalanced'
 
 
 def match_checker(char,prior):	
 	
-	matches = [('(', ')'),('[', ']'),('{', '}'),('<', '>')]
-	for opening, closing in matches:
-		if char == closing and prior == opening:
-			return "Balanced."
-	return "Unbalanced."
+    matches = [('(', ')'),('[', ']'),('{', '}'),('<', '>')]
+    for opening, closing in matches:
+        if char == closing and prior == opening:
+            return "Balanced."
+    return "Unbalanced."
 
 
 def int_to_bin(value):
 	
-	stack = Stack()
-	num = int(value)
-	binary_num = ''
+    stack = Stack()
+    num = int(value)
+    binary_num = ''
 	
-	while num > 0:
-		
-		mod = num % 2
-		stack.push(mod)
-		num = num // 2
-
-	while stack.isEmpty() == False:
-		binary_num += str(stack.pop())
-		
-	return binary_num
+    while num > 0:
+        stack.push(num % 2)
+        num = num // 2
+    while not stack.isEmpty():
+        binary_num += str(stack.pop())
+    return binary_num
 
 
 if int_to_bin(233) == '11101001':
-	print "233 equals", int_to_bin(233)
+    print "233 equals", int_to_bin(233)
 else:
-	print "Doesn't work."
+    print "Doesn't work."
